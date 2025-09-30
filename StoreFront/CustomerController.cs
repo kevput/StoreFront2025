7263c0942b4f725fa3f22dfdd133126fbaceda21
Ewout Eetesonne <ewout.eetesonne@hogent.be>
2025-09-30 21:54:58 +0200

    Initial commit

---
+++ StoreFront/CustomerController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StoreFront.Model;
+
+namespace StoreFront
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CustomerController : ControllerBase
+	{
+		private static Dictionary<int, Customer> _customers = new() { { 1, new("Gilles", new(2001, 5, 11), "gilles.penen@hogent.com") } };
+
+		[HttpGet]
+		public List<Customer> GetCustomers()
+		{
+			return _customers.Values.ToList();
+		}
+
+		[HttpPut]
+		public void StoreCustomer([FromBody] Customer customer)
+		{
+			_customers.Add(_customers.Count + 1, customer);
+		}
+
+		[HttpGet("detail")]
+		public Customer GetCustomerById([FromQuery]int id)
+		{
+			return _customers[id];
+		}
+	}
+}
---
+++ StoreFront/Model/Customer.cs
@@ -0,0 +1,16 @@
+﻿namespace StoreFront.Model
+{
+	public class Customer
+	{
+		public Customer(string name, DateTime dateOfBirth, string email)
+		{
+			Name = name;
+			DateOfBirth = dateOfBirth;
+			Email = email;
+		}
+
+		public string Name { get; }
+		public DateTime DateOfBirth { get; }
+		public string Email { get; }
+	}
+}
