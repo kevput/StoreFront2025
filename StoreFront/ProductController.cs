5a8aadaa52bbc57c71fbe868328c2314c336e4c9
kevput <147740595+kevput@users.noreply.github.com>
2025-10-06 23:17:01 +0200

    Correct the decimal

--- StoreFront/ProductController.cs
+++ StoreFront/ProductController.cs
@@ -9,13 +9,16 @@ namespace StoreFront
     [ApiController]
     public class ProductController : ControllerBase
     {
-        private static Dictionary<int, Product> _products = new() { { 1, new("Xbox","a nice toy for men",550,18,1) } };
+        private static Dictionary<int, Product> _products = new() { { 1, new("Xbox","a nice toy for men",550.50m,18,1) } };
+        
         // GET: api/<ProductController>
         [HttpGet]
         public List<Product>GetProducts()
         {
             return _products.Values.ToList();
         }
+
+
 
     }
 }
