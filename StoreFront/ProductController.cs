2b02c61577dd7acb473379c5b1cd096c392952d9
kevput <147740595+kevput@users.noreply.github.com>
2025-10-06 23:05:17 +0200

    Delete the unused junk

--- StoreFront/ProductController.cs
+++ StoreFront/ProductController.cs
@@ -17,29 +17,5 @@ namespace StoreFront
             return _products.Values.ToList();
         }
 
-        // GET api/<ProductController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<ProductController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/<ProductController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<ProductController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
